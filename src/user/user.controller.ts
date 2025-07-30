d01daa6ae8b0b150831e47fda64198ba002ac273
Pedro <peedroitalo2906@gmail.com>
2025-07-30 20:01:50 -0300

    feat: created user.controller and user.service

---
+++ src/user/user.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    const user = await this.userService.create(createUserDto);
+    const { password, ...rest } = user;
+    return rest;
+  }
+}
